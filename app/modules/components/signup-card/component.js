3e5dbd94ca8714d8b04dacd6363c32f837f67931
Ulises Ramirez-Roche <ulisesrmzroche@gmail.com>
2015-12-09 15:15:50 -0600

    Signup Card

--- app/modules/components/signup-card/component.js
+++ app/modules/components/signup-card/component.js
@@ -2,15 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   providers: Ember.computed(function(){
-    return [
-      Ember.Object.create({ name: "Facebook" }),
-      Ember.Object.create({ name: "Twitter"}),
-      Ember.Object.create({ name: "Google"}),
-    ]
+    let providers = ["Facebook", "Twitter", "Google", "Email"].map((provider)=>{
+      return Ember.Object.create({name: provider})
+    })
+    return providers;
   }),
+  willSignupWithEmail: false,
   actions: {
     signupWith(provider){
-      this.attrs.save({provider: provider});
-    }
-  }
+      if (provider.get('name') === 'email') {
+        return this._signupWithEmail()
+      }
+
+      this.createUser(provider)
+    },
+  },
+
+  _signupWithEmail(){
+    this.set('willSignupWithEmail', true);
+  },
+
+  _createUser(provider){
+    let user = this.get('model.newUser')
+    return user.save().then((user)=>{
+      console.log(user, provider)
+    })
+  },
+
 });
